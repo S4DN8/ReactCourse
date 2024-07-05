1322c1d49d13851ed1f83b47d4446811eee16062
S4DN8 <like.anime999@gmail.com>
2024-07-05 21:50:44 +0300

    Change all code

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,39 +1,82 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
 
-const example: string = 'This is an example';
-console.log(example);
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
 
-function App() {
-  const [count, setCount] = useState(0);
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      ;
-    </>
-  );
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.log(`Error info: ${error} ${errorInfo} `);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1 id="err-text">Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+interface AppProps {}
+
+interface AppState {
+  searchTerm: string;
+  results: { name: string; url: string }[];
+  isLoading: boolean;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      results: [],
+      isLoading: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      searchTerm: e.target.value.replace(/\s/g, `%`),
+    });
+  }
+
+  handleClick() {
+    console.log(`${this.state.searchTerm}`);
+  }
+
+  render() {
+    return (
+      <ErrorBoundary>
+        <section id="search">
+          <input id="input-search" onChange={this.handleChange}></input>
+          <button id="btn-search" onClick={this.handleClick}>
+            Search
+          </button>
+        </section>
+        <section id="main"></section>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
